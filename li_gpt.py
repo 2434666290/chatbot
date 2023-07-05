d67593457ca8944fdc6ee762fe5f7fc38601049a
2434666290 <131752374+2434666290@users.noreply.github.com>
2023-07-05 16:22:35 +0800

    Update li_gpt.py

--- li_gpt.py
+++ li_gpt.py
@@ -1,4 +1,3 @@
-# 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
 import openai
 import streamlit as st
 
@@ -6,101 +5,124 @@ import streamlit as st
 st.set_page_config(page_title="Li-chatbot", page_icon="robot_face:")
 st.markdown("<h1 style='text-align: center;'>chatbot created by Li</h1>", unsafe_allow_html=True)
 
-# Set API key
+# Set API key and choose a model and choose a tool
 openai.api_key = st.sidebar.text_input('KEY')
-
-# Initialise session state variables
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = [
-        {"role": "system", "content": "You are a helpful assistant."}
-    ]
-if 'model_name' not in st.session_state:
-    st.session_state['model_name'] = []
-if 'cost' not in st.session_state:
-    st.session_state['cost'] = []
-if 'total_tokens' not in st.session_state:
-    st.session_state['total_tokens'] = []
-if 'total_cost' not in st.session_state:
-    st.session_state['total_cost'] = 0.0
-
-# Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
 model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
-counter_placeholder = st.sidebar.empty()
-counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
-clear_button = st.sidebar.button("Clear Conversation", key="clear")
+choose = st.sidebar.selectbox('Tools', ['AI communication', 'AI painting'])
 
-# Map model names to OpenAI model IDs
-if model_name == "GPT-3.5":
-    model = "gpt-3.5-turbo"
-else:
-    model = "gpt-4"
 
-# reset everything
-if clear_button:
-    st.session_state['generated'] = []
-    st.session_state['past'] = []
-    st.session_state['messages'] = [
-        {"role": "system", "content": "You are a helpful assistant."}
-    ]
-    st.session_state['number_tokens'] = []
-    st.session_state['model_name'] = []
-    st.session_state['cost'] = []
-    st.session_state['total_cost'] = 0.0
-    st.session_state['total_tokens'] = []
+if choose == 'AI communication':
+    # Initialise session state variables
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = [
+            {"role": "system", "content": "You are a helpful assistant."}
+        ]
+    if 'model_name' not in st.session_state:
+        st.session_state['model_name'] = []
+    if 'cost' not in st.session_state:
+        st.session_state['cost'] = []
+    if 'total_tokens' not in st.session_state:
+        st.session_state['total_tokens'] = []
+    if 'total_cost' not in st.session_state:
+        st.session_state['total_cost'] = 0.0
+
+    # Sidebar - show total cost of current conversation, and let user clear the current conversation
+    counter_placeholder = st.sidebar.empty()
     counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+    clear_button = st.sidebar.button("Clear Conversation", key="clear")
+
+    # Map model names to OpenAI model IDs
+    if model_name == "GPT-3.5":
+        model = "gpt-3.5-turbo"
+    else:
+        model = "gpt-4"
+
+    # reset everything
+    if clear_button:
+        st.session_state['generated'] = []
+        st.session_state['past'] = []
+        st.session_state['messages'] = [
+            {"role": "system", "content": "You are a helpful assistant."}
+        ]
+        st.session_state['number_tokens'] = []
+        st.session_state['model_name'] = []
+        st.session_state['cost'] = []
+        st.session_state['total_cost'] = 0.0
+        st.session_state['total_tokens'] = []
+        counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
 
 
-# generate a response
-def generate_response(prompt):
-    st.session_state['messages'].append({"role": "user", "content": prompt})
+    # generate a response
+    def generate_response(prompt):
+        st.session_state['messages'].append({"role": "user", "content": prompt})
 
-    completion = openai.ChatCompletion.create(
-        model=model,
-        messages=st.session_state['messages']
-    )
-    response = completion.choices[0].message.content
-    st.session_state['messages'].append({"role": "assistant", "content": response})
+        completion = openai.ChatCompletion.create(
+            model=model,
+            messages=st.session_state['messages']
+        )
+        response = completion.choices[0].message.content
+        st.session_state['messages'].append({"role": "assistant", "content": response})
 
-    # print(st.session_state['messages'])
-    total_tokens = completion.usage.total_tokens
-    prompt_tokens = completion.usage.prompt_tokens
-    completion_tokens = completion.usage.completion_tokens
-    return response, total_tokens, prompt_tokens, completion_tokens
+        # print(st.session_state['messages'])
+        total_tokens = completion.usage.total_tokens
+        prompt_tokens = completion.usage.prompt_tokens
+        completion_tokens = completion.usage.completion_tokens
+        return response, total_tokens, prompt_tokens, completion_tokens
 
 
-# container for chat history
-response_container = st.container()
+    # container for chat history
+    response_container = st.container()
 
-with st.form(key='my_form', clear_on_submit=True):
-    user_input = st.text_area("You:", key='input', height=100)
-    submit_button = st.form_submit_button(label='Send')
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
 
-if submit_button and user_input:
-    with st.spinner('Running...'):
-        output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
-        st.session_state['past'].append(user_input)
-        st.session_state['generated'].append(output)
-        st.session_state['model_name'].append(model_name)
-        st.session_state['total_tokens'].append(total_tokens)
-    
-        # from https://openai.com/pricing#language-models
-        if model_name == "GPT-3.5":
-            cost = total_tokens * 0.002 / 1000
-        else:
-            cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
-    
-        st.session_state['cost'].append(cost)
-        st.session_state['total_cost'] += cost
+    if submit_button and user_input:
+        with st.spinner('Running...'):
+            output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+            st.session_state['model_name'].append(model_name)
+            st.session_state['total_tokens'].append(total_tokens)
 
-if st.session_state['generated']:
-    with response_container:
-        for i in range(len(st.session_state['generated'])):
-            st.write("\U0001F60A:",st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-            st.write("\U0001F916:",st.session_state["generated"][i], key=str(i))
-            st.write(
-                f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
-            counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+            # from https://openai.com/pricing#language-models
+            if model_name == "GPT-3.5":
+                cost = total_tokens * 0.002 / 1000
+            else:
+                cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+
+            st.session_state['cost'].append(cost)
+            st.session_state['total_cost'] += cost
+
+    if st.session_state['generated']:
+        with response_container:
+            for i in range(len(st.session_state['generated'])):
+                st.write("\U0001F60A:",st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                st.write("\U0001F916:",st.session_state["generated"][i], key=str(i))
+                st.write(
+                    f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
+                counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+
+elif choose == 'AI painting':
+
+    with st.form(key='my_image_form', clear_on_submit=True):
+        def ChatGPT_AI_DrawBot(input):
+            if input:
+                response = openai.Image.create(
+                    prompt=input,
+                    n=1,
+                    size="1024x1024"
+                )
+                image_url = response['data'][0]['url']
+                return image_url
+
+        input = st.text_area("what's you want to paint")
+        #st.image(ChatGPT_AI_DrawBot(input))
+        if st.form_submit_button('start'):
+            with st.spinner('Loading Image...'):
+                st.image(ChatGPT_AI_DrawBot(input), caption='Generated Image')
+
